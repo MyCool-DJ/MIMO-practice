82f0871fee1a4b667997c5bfdbf15a4d29df64b5
Michael Spicer <>
2021-05-01 08:54:26 +0000

    New task; Car Rental tracker

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-score_1 = 98.5
-score_2 = 97.6
+total = 12
+rented_cars = 6
+available = total - rented_cars
 
-if score_1 > score_2:
-  print("Test score 1 is higher")
-elif score_2 > score_1:
-  print("Test score 2 is higher")
-else:
-  print("The test scores are equal")
+print(f"{available} available cars")
+
+rented_cars += 3
+available = total - rented_cars
+
+print(f"{available} available cars")
