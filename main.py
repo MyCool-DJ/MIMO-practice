e4f9e0282af8d6b673b711f179063bd68def0c7a
MichaelSpicer1 <>
2021-05-01 08:49:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+score_1 = 98.5
+score_2 = 97.6
+
+if score_1 > score_2:
+  print("Test score 1 is higher")
+elif score_2 > score_1:
+  print("Test score 2 is higher")
+else:
+  print("The test scores are equal")
